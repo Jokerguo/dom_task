3c8cf4d4878e8a2a88748c333383cbe3a7829bc2
jokerguo <1977036313@qq.com>
2020-01-09 15:50:22 +0800

    dom_task

---
+++ src/dom.js
@@ -0,0 +1,16 @@
+window.dom = {
+    find(selector){
+       return document.querySelectorAll(selector)
+    },
+
+    style(node,name,value){
+        node.style[name] = value
+    },
+
+    each(nodeList , fn){
+        for(let i=0;i<nodeList.length;i++){
+            fn.call(null,nodeList[i])
+        }
+    }
+
+}
